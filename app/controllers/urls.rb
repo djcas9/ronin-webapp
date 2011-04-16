91b531d074b1ff414382c36991e237ea0298f202
Postmodern <postmodern.mod3@gmail.com>
2011-04-16 05:45:38 -0700

    Added the urls controller.

---
+++ app/controllers/urls.rb
@@ -0,0 +1,17 @@
+Ronin::WebApp.controllers :urls do
+
+  provides :html, :xml, :yaml, :json
+
+  get :index do
+    @urls = URL.all
+
+    render_resource @urls, 'urls/index'
+  end
+
+  get :show, :map => '/urls/:id' do
+    @url = URL.get(params[:id])
+
+    render_resource @url, 'urls/show'
+  end
+
+end
