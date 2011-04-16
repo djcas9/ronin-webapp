43eac4421f089ffcd673ae05fe9bfabf9cece7f0
Postmodern <postmodern.mod3@gmail.com>
2011-04-16 05:00:03 -0700

    Added the open_ports controller.

---
+++ app/controllers/open_ports.rb
@@ -0,0 +1,17 @@
+Ronin::WebApp.controllers :open_ports do
+
+  provides :html, :xml, :yaml, :json
+
+  get :index do
+    @open_ports = OpenPort.all
+
+    render_resource @open_ports, '/open_ports/index'
+  end
+
+  get :show, :map => '/open_ports/:id' do
+    @open_port = OpenPort.get(params[:id])
+
+    render_resource @open_port, '/open_ports/show'
+  end
+
+end
